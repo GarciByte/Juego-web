bc759d998cb17a44cfb55a0cef53c9720e60d593
David <145496969+GarciByte@users.noreply.github.com>
2025-02-25 14:45:31 +0100

    Actualizar despliegue

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     // en producción
-    apiUrl: "memory-game.runasp.net/api/",
-    apiImg: "memory-game.runasp.net/",
-    socketUrl: 'wss://memory-game.runasp.net/socket'
+    apiUrl: "https://memory-game.runasp.net/api/",
+    apiImg: "https://memory-game.runasp.net/",
+    socketUrl: "wss://memory-game.runasp.net/socket"
   };
   
